cdf216e629736d94545cf62d57bf1123c9f3e16b
Ninutsa777 <ninutsasikharulidze@gmail.com>
2024-11-25 16:51:11 +0000

    nnn

---
+++ https:/github.com/Ninutsa777/ninutsanew.js
@@ -0,0 +1,30 @@
+
+let ninutsa ={
+    name:"ninutsa",
+    "LastName":"Sikharulidze",
+    "phone":"3242342",
+    Adress:"TBilisi",
+    "Age":"31"
+}
+console.log(ninutsa)
+let x=30, y=5;
+c=x/y
+console.log(c)
+
+let k=20, e=20
+g=k=e 
+console.log (g)
+
+let M=50, L=20
+F=M==L
+console.log (F)
+
+ninutsa=31;
+
+if (ninutsa>18) {
+    console.log ("ninutsa სრულწლოვანია")
+}
+
+else {
+    console.log ("ninutsa არასრულწლოვანია")
+}
