01ad4be125dc20a25136729042742180c02d4c13
Ninutsa777 <ninutsasikharulidze@gmail.com>
2024-11-26 06:51:29 +0000

    s

--- ninjsnew.js
+++ ninjsnew.js
@@ -1,5 +1,9 @@
-let x=87, y=65;
 
-c=x+y
 
-console.log (c)
+let student= {
+    FirsName: "ninutsa",
+    LastName:"Sikharulidze",
+     age: 31, email:"dddd" ,
+     marks: 100
+    }
+console.log (student.FirsName)
