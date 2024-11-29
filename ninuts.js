20349b403ed31be5f676e30adab4d585b5730535
Ninutsa777 <ninutsasikharulidze@gmail.com>
2024-11-29 06:55:36 +0000

    gg

--- ninuts.js
+++ ninuts.js
@@ -1,3 +1,12 @@
 let fruits = ["Apple","Banana","Orange"]
 
 console.log (fruits)
+
+
+let fruite= ["Apple","Banana","Orange"]
+
+console.log (fruite)
+
+fruite.unshift ("Grapes")
+
+console.log (fruite)
