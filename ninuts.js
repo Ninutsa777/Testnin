2664ed44f436a8d7342e2bedb543989c56355688
Ninutsa777 <ninutsasikharulidze@gmail.com>
2024-11-29 07:14:00 +0000

    ggg

--- ninuts.js
+++ ninuts.js
@@ -1,3 +1,17 @@
+let person = {
+    FirstName:"Lika",
+    LastName: "Kldiashvili",
+    Age:6
+}
+
+console.log (person.FirstName)
+console.log (person.LastName)
+
+
+
+
+
+
 let fruits = ["Apple","Banana","Orange"]
 
 console.log (fruits)
