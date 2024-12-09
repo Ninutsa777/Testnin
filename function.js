7178ca6f95ca48bedb2db0731868a4c7b195300b
Ninutsa777 <ninutsasikharulidze@gmail.com>
2024-12-09 07:56:45 +0000

    ss

--- function.js
+++ function.js
@@ -5,3 +5,14 @@ function sum (m, n) {
 
 let result=sum (20,10)
 console. log (result);
+
+function fullname (person){
+    let results="sruli saxeli=" + person.firstName+ person.lastName
+    return (results)
+}
+let girl={
+    firstName:"Lika",
+    lastName:"Kldiashvili"
+}
+let srulisakheli=fullname(girl)
+console.log(srulisakheli)
