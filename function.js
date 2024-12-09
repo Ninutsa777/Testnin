ab4cede06200bdcd93087ddf8e2169b719180e54
Ninutsa777 <ninutsasikharulidze@gmail.com>
2024-12-09 15:18:16 +0000

    sass

--- function.js
+++ function.js
@@ -6,6 +6,20 @@ function sum (m, n) {
 let result=sum (20,10)
 console. log (result);
 
+
+
+function srulisakhelidagvari(person){
+    console.log ("sruli saxeli=",person.firstName, person.lastName)
+} 
+let person={
+    firstName: "ninutsa",
+    lastName:"sikharulidze"
+}
+fullname(person)
+
+
+
+
 function fullname (person){
     let results="sruli saxeli=" + person.firstName+ person.lastName
     return (results)
